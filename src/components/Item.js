896b973b186ba4ec6d67c41b09be0b1dae2471a7
Liam Simmons <liam.simmons@powerhrg.com>
2024-06-11 14:01:04 -0400

    Added state usage

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [count, setCount] = useState(false)
+  const updateClass = count ? 'in-cart' : ''
+  const updateButtonText = count ? 'Remove From Cart' : 'Add to Cart'
   return (
-    <li className="">
+    <li className={ updateClass }>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={() => setCount(count => !count)}>
+        { updateButtonText }
+      </button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+
+  const [count, setCount] = useState('All')
+  const filterByCategory = count === 'All' ? items : items.filter(item => item.category === count)
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select onChange={(event) => setCount(event.target.value)} name="filter">
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,9 +17,9 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
-        ))}
+        { filterByCategory.map((item) => (
+    <Item key={item.id} name={item.name} category={item.category} />
+  )) }
       </ul>
     </div>
   );
